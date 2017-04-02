e4e75bea0a367c50ab91d7d709fa8e9f62731bc7
shehryarAhmed <shehreyarahmed5@gmail.com>
2017-04-02 18:41:20 +0500

    add model class for blog psot

--- SimpleBlogApp/app/src/main/java/com/example/android/simpleblogapp/MainActivity.java
+++ SimpleBlogApp/app/src/main/java/com/example/android/simpleblogapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.android.simpleblogapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ SimpleBlogApp/app/src/main/java/com/example/android/simpleblogapp/model/BlogPost.java
@@ -0,0 +1,34 @@
+package com.example.android.simpleblogapp.model;
+
+/**
+ * Created by android on 4/2/2017.
+ */
+
+public class BlogPost {
+    private int thumnail;
+    private String title;
+    private String description;
+
+    public BlogPost(int thumnail, String title, String description) {
+        this.thumnail = thumnail;
+        this.title = title;
+        this.description = description;
+    }
+
+    public BlogPost(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public int getThumnail() {
+        return thumnail;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
