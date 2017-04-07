617386869a6472d92cb0c5c5ebd199058c49a307
shehryarAhmed <shehreyarahmed5@gmail.com>
2017-04-08 00:37:56 +0500

    working

--- SimpleBlogApp/app/src/main/java/com/example/android/simpleblogapp/PostActivity.java
+++ SimpleBlogApp/app/src/main/java/com/example/android/simpleblogapp/PostActivity.java
@@ -3,8 +3,11 @@ package com.example.android.simpleblogapp;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.net.Uri;
+import android.os.storage.StorageVolume;
+import android.support.v4.text.TextUtilsCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.animation.Interpolator;
 import android.widget.Toast;
@@ -16,6 +19,9 @@ public class PostActivity extends AppCompatActivity {
     binding_post bind_post;
 
     private static final int GALLERY_REQUEST = 1;
+
+    private Uri imageUri = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +40,19 @@ public class PostActivity extends AppCompatActivity {
         bind_post.postBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                sendPost();
 
 
             }
         });
+    }
+
+    private void sendPost() {
+        String title_val = bind_post.postTitle.getText().toString().trim();
+        String desc_val = bind_post.postDesc.getText().toString().trim();
+        if (!TextUtils.isEmpty(title_val) && !TextUtils.isEmpty(desc_val) && imageUri != null){
+        }
+
     }
 
 
@@ -45,7 +60,7 @@ public class PostActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == GALLERY_REQUEST && resultCode == RESULT_OK){
-            Uri imageUri = data.getData();
+            imageUri = data.getData();
             bind_post.postImage.setImageURI(imageUri);
         }
     }
