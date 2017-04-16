77e2dd99d88f576382a38653666bc269b3fb9954
shehryarAhmed <shehreyarahmed5@gmail.com>
2017-04-16 21:22:51 +0500

    image crop

--- SimpleBlogApp/app/src/main/java/com/example/android/simpleblogapp/userDetail/ProfileSetting.java
+++ SimpleBlogApp/app/src/main/java/com/example/android/simpleblogapp/userDetail/ProfileSetting.java
@@ -1,28 +1,79 @@
 package com.example.android.simpleblogapp.userDetail;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
 import com.example.android.simpleblogapp.R;
+import com.example.android.simpleblogapp.databinding.bindprofsetting;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class ProfileSetting extends AppCompatActivity {
 
-    ImageButton imageButton;
-    EditText editText;
-    Button button;
+    bindprofsetting bind;
+
+    private static final int GALLERY_REQUEST = 1;
+
+    private Uri imageUri = null;
+
+    private DatabaseReference mDatabase;
+
+    private StorageReference mStroaoge;
+
+    private ProgressDialog mProgress;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_setting);
 
-        imageButton = (ImageButton) findViewById(R.id.displayImg);
-        editText = (EditText) findViewById(R.id.displayName);
-        button = (Button) findViewById(R.id.updatechanges);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
+        mStroaoge = FirebaseStorage.getInstance().getReference();
+
+        mProgress = new ProgressDialog(this);
+
+        bind.displayImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/jpeg");
+                galleryIntent.putExtra(Intent.EXTRA_LOCAL_ONLY,true);
+                startActivityForResult(Intent.createChooser(galleryIntent,"Complete Action Using"),GALLERY_REQUEST);
+            }
+        });
 
 
+        bind.updatechanges.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == GALLERY_REQUEST && resultCode == RESULT_OK){
+            imageUri = data.getData();
+            //bind.displayImg.setImageURI(imageUri);
+        }
     }
 }
